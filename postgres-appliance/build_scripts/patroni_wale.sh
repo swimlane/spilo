169d6fd7173a98a5c4e85f7c292fdb1968f7e3a7
xRichieGarcia <richieradda22@gmail.com>
2024-04-02 20:58:15 -0500

    use pip for python pkgs

--- postgres-appliance/build_scripts/patroni_wale.sh
+++ postgres-appliance/build_scripts/patroni_wale.sh
@@ -13,38 +13,44 @@ BUILD_PACKAGES=(python3-pip python3-wheel python3-dev git patchutils binutils gc
 apt-get update
 
 # install most of the patroni dependencies from ubuntu packages
-apt-cache depends patroni \
-        | sed -n -e 's/.* Depends: \(python3-.\+\)$/\1/p' \
-        | grep -Ev '^python3-(sphinx|etcd|consul|kazoo|kubernetes)' \
-        | xargs apt-get install -y "${BUILD_PACKAGES[@]}" python3-pystache python3-requests
+# apt-cache depends patroni \
+#         | sed -n -e 's/.* Depends: \(python3-.\+\)$/\1/p' \
+#         | grep -Ev '^python3-(sphinx|etcd|consul|kazoo|kubernetes)' \
+#         | xargs apt-get install -y "${BUILD_PACKAGES[@]}" python3-pystache python3-requests
+
+apt-get install -y "${BUILD_PACKAGES[@]}"
 
 pip3 install setuptools
 
+# use pip instead of ubuntu pkgs
+# requirements were sourced from the pip freeze of a spilo container and updated using pip-tools
+pip3 install -r /builddeps/requirements.txt
+
 if [ "$DEMO" != "true" ]; then
     EXTRAS=",etcd,consul,zookeeper,aws"
-    apt-get install -y \
-        python3-etcd \
-        python3-consul \
-        python3-kazoo \
-        python3-boto \
-        python3-boto3 \
-        python3-botocore \
-        python3-cachetools \
-        python3-cffi \
-        python3-gevent \
-        python3-pyasn1-modules \
-        python3-rsa \
-        python3-s3transfer \
-        python3-swiftclient
+    # apt-get install -y \
+    #     python3-etcd \
+    #     python3-consul \
+    #     python3-kazoo \
+    #     python3-boto \
+    #     python3-boto3 \
+    #     python3-botocore \
+    #     python3-cachetools \
+    #     python3-cffi \
+    #     python3-gevent \
+    #     python3-pyasn1-modules \
+    #     python3-rsa \
+    #     python3-s3transfer \
+    #     python3-swiftclient
 
-    find /usr/share/python-babel-localedata/locale-data -type f ! -name 'en_US*.dat' -delete
+    # find /usr/share/python-babel-localedata/locale-data -type f ! -name 'en_US*.dat' -delete
 
     pip3 install filechunkio protobuf \
             'git+https://github.com/zalando-pg/wal-e.git#egg=wal-e[aws,google,swift]' \
             'git+https://github.com/zalando/pg_view.git@master#egg=pg-view'
 
     # https://github.com/wal-e/wal-e/issues/318
-    sed -i 's/^\(    for i in range(0,\) num_retries):.*/\1 100):/g' /usr/lib/python3/dist-packages/boto/utils.py
+    sed -i 's/^\(    for i in range(0,\) num_retries):.*/\1 100):/g' /usr/local/lib/python3.10/dist-packages/boto/utils.py
 else
     EXTRAS=""
 fi
